3092152fe2c2be541ff05d346e91c47a14b3c5ed
Vladimir Reshetsko <44954340+ejararv@users.noreply.github.com>
2021-05-04 22:33:41 +0200

    Proto

--- 4_Prototype.js
+++ 4_Prototype.js
@@ -1,10 +1,25 @@
-class Prototyp {
-  constructor(id) {
-    this.Id = id;
-  }
-  Klonuj() {
-   
+const Prototyp = {
+  id: "id",
+
+  klonuj() {
+    console.log(`Sklonowano: ${this.Id}`);
   }
 }
 
-console.log(Prototyp);
+const PrototypX = Object.create(Prototyp, {
+  Id: {
+    value: "2",
+  }
+})
+
+const PrototypY = Object.create(Prototyp, {
+  Id: {
+    value: "1",
+  }
+})
+
+console.log(PrototypY.__proto__ === Prototyp)
+console.log(PrototypX.__proto__ === Prototyp)
+
+PrototypX.klonuj()
+PrototypY.klonuj()
